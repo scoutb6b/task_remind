f5fef7db673f706d48c3542fc1d1dd44fc718088
kbtkt <kubota@atelier-bits.co.jp>
2024-06-10 10:31:01 +0900

    ✨ スキーマとモデルを定義

---
+++ src/models/remind.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document } from "mongoose";
+
+export interface Remind {
+  company: string;
+  startDate: string;
+  endDate: string;
+  content: string;
+}
+export interface RemindDoc extends Remind, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const remindScema = new mongoose.Schema<RemindDoc>(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const RemindModel =
+  mongoose.models.Remind || mongoose.model("Remind", remindScema);
