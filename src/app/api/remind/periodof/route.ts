d994f858a5f5a89cd9ae6ae2acf5b51e2475e15c
kbtkt <kubota@atelier-bits.co.jp>
2024-07-11 11:35:48 +0900

    ✨ 期限内のページ作成、api作成

---
+++ src/app/api/remind/periodof/route.ts
@@ -0,0 +1,27 @@
+import { connectDb } from "@/lib/database";
+import { RemindDoc, RemindModel } from "@/models/remind";
+import dayjs from "dayjs";
+import { NextResponse } from "next/server";
+
+// READ
+export const GET = async () => {
+  try {
+    await connectDb();
+    const today = dayjs().format("YYYY-MM-DD");
+
+    const PeriodofRemindTasks: RemindDoc[] = await RemindModel.find({
+      endDate: { $gt: today },
+    });
+    console.log(PeriodofRemindTasks);
+
+    return NextResponse.json(
+      { message: "取得成功", tasks: PeriodofRemindTasks },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "取得に失敗しました" }, { status: 500 });
+  }
+};
+
+export const dynamic = "force-dynamic";
