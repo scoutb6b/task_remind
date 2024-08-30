79c6a223a07d76acf0bfbb6aa0053cc7f23a0aa0
kbtkt <kubota@atelier-bits.co.jp>
2024-08-30 15:46:16 +0900

    zod導入

--- src/components/main/form/InsertForm.tsx
+++ src/components/main/form/InsertForm.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { FormState, insertTask } from "@/actions/task";
+import { insertTask, State } from "@/actions/task";
 import { useFormState, useFormStatus } from "react-dom";
 
 export const InsertForm = () => {
-  const initialState: FormState = { error: "" };
+  const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useFormState(insertTask, initialState);
-
   const HandleSubmit = () => {
     const { pending } = useFormStatus();
     return (
       <button
         type="submit"
-        className="mt-8 px-20 py-2 block mx-auto rounded-lg bg-green-800 text-white shadow-md hover:shadow-none hover: px-2opacity-75 disabled:bg-gray-400"
+        className="mt-8 px-20 py-2 block mx-auto rounded-lg border-2 bg-green-800 text-white  
+        hover:bg-slate-50 hover:border-green-800 hover:border-2 hover:text-green-800  disabled:bg-gray-400 transition-all duration-500"
         disabled={pending}
       >
         追加する
@@ -23,9 +23,9 @@ export const InsertForm = () => {
   return (
     <div>
       <form action={formAction} className="">
-        <div className="flex flex-col mt-4 font-medium text-lg w-3/4 mx-auto">
+        <div className="flex flex-col mt-5 mb-10 font-medium text-lg w-3/4 mx-auto">
           <label htmlFor="" className="font-semibold">
-            会社名
+            タスク名
           </label>
           <input
             type="text"
@@ -33,10 +33,18 @@ export const InsertForm = () => {
             id=""
             className="mt-2 py-1 px-2 rounded-lg "
           />
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.company?.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col mt-4 font-medium text-lg w-3/4 mx-auto">
+
+        <div className="flex flex-col mt-5 mb-10 font-medium text-lg w-3/4 mx-auto">
           <label htmlFor="" className="font-semibold">
-            保守開始日(例:2024-10-01)
+            開始日(例:2024-10-01)
           </label>
           <input
             type="text"
@@ -44,10 +52,17 @@ export const InsertForm = () => {
             id=""
             className="mt-2 py-1 px-2 rounded-lg "
           />
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.startDate?.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col mt-4 font-medium text-lg w-3/4 mx-auto">
+        <div className="flex flex-col mt-5 mb-10 font-medium text-lg w-3/4 mx-auto">
           <label htmlFor="" className="font-semibold">
-            保守終了日(例:2025-04-10)
+            終了日(例:2025-04-10)
           </label>
           <input
             type="text"
@@ -55,8 +70,15 @@ export const InsertForm = () => {
             id=""
             className="mt-2 py-1 px-2 rounded-lg "
           />
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.endDate?.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col mt-4 font-medium text-lg w-3/4 mx-auto">
+        <div className="flex flex-col mt-5 mb-10 font-medium text-lg w-3/4 mx-auto">
           <label htmlFor="" className="font-semibold">
             備考
           </label>
@@ -67,9 +89,6 @@ export const InsertForm = () => {
           ></textarea>
         </div>
         <HandleSubmit />
-        {state.error && (
-          <p className="mt-2 text-red-500 text-sm">{state.error}</p>
-        )}
       </form>
     </div>
   );
